6d147e2599b59e0498e17a3653738b947ef0eeed
vuminh98 <vuminhtran027@gmail.com>
2023-04-04 20:50:56 +0700

    a

--- quote/src/main/java/Database/quote/repository/QuoteRepository.java
+++
@@ -1,9 +0,0 @@
-package Database.quote.repository;
-
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class QuoteRepository extends JpaRepositoriesAutoConfiguration<Quote> {
-
-}
