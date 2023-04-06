7a5c0e16cc3b08ad790939a44e9b1047c8f5f0e3
Minh Vu <vuminhtran027@gmail.com>
2023-04-06 11:55:10 +0700

    aa

--- ReflectionDemo/src/main/java/Database/ReflectionDemo/Main.java
+++ ReflectionDemo/src/main/java/Database/ReflectionDemo/Main.java
@@ -33,5 +33,16 @@ public class Main {
         //setAccessible: thiết lập quyền truy cập của 1 trường, phương thức
         color.set(car, "blue");
         System.out.println(car); //Car [name=Porsche, quantity=2, color=blue, isAvaiable=true]
+        
+        
+//        Lớp Field trong Java Reflection được sử dụng để thao tác với các trường (fields) của một đối tượng. 
+//        Với lớp Field, bạn có thể lấy ra giá trị của một trường, thiết lập giá trị cho một trường, hoặc lấy thông tin về kiểu dữ liệu và phạm vi truy cập của trường. 
+//        Để làm việc với một trường cụ thể, bạn có thể sử dụng phương thức getDeclaredField() để lấy đối tượng Field tương ứng với trường đó.
+        
+//        Lớp Method trong Java Reflection được sử dụng để thao tác với các phương thức (methods) của một đối tượng. 
+//        Với lớp Method, bạn có thể gọi một phương thức cụ thể, lấy thông tin về tên phương thức, kiểu trả về và tham số của phương thức đó. 
+//        Để làm việc với một phương thức cụ thể, bạn có thể sử dụng phương thức getDeclaredMethod() để lấy đối tượng Method tương ứng với phương thức đó.
+        
+        
     }
 }
